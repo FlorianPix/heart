7f2443f66505364acdaca8cbf17e80fbdc26fa0c
RatedARRR <florian.pix.97@outlook.com>
2020-01-05 12:09:13 +0100

    removed wrong repo

--- leaf/convert_svg.py
+++
@@ -1,6 +0,0 @@
-txt = open("path.txt", "r")
-txt = txt.read().replace(' ', '\n')
-txt = txt.replace('c', '\n')
-
-file = open("path2.txt", "w")
-file.write(txt)
--- leaf/output/convert.bat
+++
@@ -1,4 +0,0 @@
-FOR /L %%A IN (1,1,640) DO (
-    magick convert gif-%%A.png gif-%%A.gif
-)
-del *.png
